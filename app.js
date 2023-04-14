9a56e824f019d10c02e7151e01ae9da586181b6e
Sean Zhou <51042748+xiaoShuZhou@users.noreply.github.com>
2023-04-14 12:25:59 +0300

    finish ex 4.21

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
 
 const mongoose = require('mongoose')
+const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
+const middleware = require('./utils/middleware')
 
 mongoose
   .connect(config.MONGODB_URI)
@@ -19,5 +22,10 @@ mongoose
 app.use(cors())
 app.use(express.static('build'))
 app.use(express.json())
+app.use(middleware.tokenExtractor)
+// app.use(middleware.tokenValidator)
+
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 module.exports = app
